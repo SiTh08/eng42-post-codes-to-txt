656288a6648ee5fadf0c3df7ef89e3407c2bfd80
Sivaharan Thevipagan <SThevipagan@spartaglobal.com>
2019-10-02 17:09:43 +0100

    completed the API get request for postcode.io to look up a postcode

--- API_get_call.py
+++ API_get_call.py
@@ -1,4 +1,12 @@
+import requests
 
+request_postcode = requests.get('http://api.postcodes.io/postcodes/IG12TP')
+
+post_code_dict = request_postcode.json()
+
+print(post_code_dict['result']['postcode'])
+print(post_code_dict['result']['country'])
+print(post_code_dict['result']['nhs_ha'])
 
 
 
